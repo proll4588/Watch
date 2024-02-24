8f6bf4fee5eefd2bc12a89e4116e479d7c62e093
Kostya <prol4588@gmail.com>
2024-02-24 14:36:41 +0700

    complete MyMoviesPage

--- app/src/main/java/com/example/watch/pages/MyMoviesPage.kt
+++ app/src/main/java/com/example/watch/pages/MyMoviesPage.kt
@@ -1,11 +1,18 @@
 package com.example.watch.pages
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Movie
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,10 +24,16 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.watch.ViewModels.MyMoviesViewModel
+import com.example.watch.components.IconWithText
 import com.example.watch.components.MovieCard
+import com.example.watch.data.database.entity.Movie
 
 @SuppressLint("FlowOperatorInvokedInComposition")
 @OptIn(ExperimentalMaterial3Api::class)
@@ -30,6 +43,29 @@ fun MyMoviesPage(
     viewModel: MyMoviesViewModel = hiltViewModel()
 ) {
     val movies = viewModel.movies.collectAsState(initial = emptyList())
+    val selectedMovies = remember {
+        mutableStateListOf<Movie>()
+    }
+
+    fun isSelected(movie: Movie): Boolean {
+        val index = selectedMovies.indexOf(movie)
+        return index != -1
+    }
+
+    fun toggleSelect(movie: Movie) {
+        val index = selectedMovies.indexOf(movie)
+        if (index == -1) {
+            selectedMovies.add(movie)
+        } else {
+            selectedMovies.removeAt(index)
+        }
+    }
+
+    fun handleDelete() {
+        selectedMovies.forEach { viewModel.removeMovie(it) }
+        selectedMovies.clear()
+    }
+
 
     Scaffold(
         floatingActionButton = {
@@ -44,7 +80,7 @@ fun MyMoviesPage(
                     titleContentColor = MaterialTheme.colorScheme.primary
                 ),
                 actions = {
-                    IconButton(onClick = { /* do something */ }) {
+                    IconButton(onClick = { handleDelete() }) {
                         Icon(
                             imageVector = Icons.Filled.Delete,
                             contentDescription = "Localized description",
@@ -54,20 +90,41 @@ fun MyMoviesPage(
                 },
                 title = {
                     Text("Movies to Watch")
-                }
-            )
-        }
-    ) { padding ->
-        LazyColumn(
-            modifier = Modifier.padding(padding),
-        ) {
-            items(movies.value.size) { index ->
-                val movie = movies.value[index]
+                },
 
-                MovieCard(
-                    movie = movie,
-                    onClick = {}
                 )
+        },
+        contentWindowInsets = WindowInsets(left = 8.dp, right = 8.dp)
+    ) { padding ->
+        if (movies.value.isEmpty()) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ) {
+                IconWithText(
+                    imageVector = Icons.Filled.Movie,
+                    text = "There are currently no movies in your watch list. Tap the button below to get started!"
+                )
+            }
+        } else {
+            LazyColumn(
+                modifier = Modifier.padding(padding),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+            ) {
+                items(movies.value.size) { index ->
+                    val movie = movies.value[index]
+
+                    MovieCard(
+                        movie = movie,
+                        onClick = { toggleSelect(movie) },
+                    ) {
+                        Checkbox(
+                            checked = isSelected(movie),
+                            onCheckedChange = { toggleSelect(movie) })
+                    }
+                }
             }
         }
     }
